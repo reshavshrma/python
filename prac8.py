354075941cc45a4b863d9ddda7d57149156474c1
Unaccessed <2022bit061@sggs.ac.in>
2024-10-10 23:43:22 +0530

    prac8

--- prac8.py
+++ prac8.py
@@ -1,31 +1,102 @@
+''' 
+04-10-2024 => def get_even_odd_count(list) - list is the list of integers objects
 '''
-Create a count function which will return total no of palindrome sequences 
-in a list object, there could be any no of list tuple string integer element
-or reference of these elements in that list
-'''
- 
-def is_palindrome(item):
-	str_item = str(item)
-	return str_item == str_item[::-1]
-	
-def count_palindrome(my_list):
-	count = 0
-	regNo = 11
-	
-	for item in my_list:
-		if isinstance(item, (list, tuple)):
-			for sub_item in item:
-				if len(str(sub_item)) % regNo == 7:
-					count += is_palindrome(sub_item)
-		else:
-			if len(str(item)) % regNo == 7 and is_palindrome(item):
-				count += 1
-	return count 
-	
-test_list_1 = [121, "abcba", 23432, [11, 22, 33, 44, 45654], ("madam", "testtset", 1234321)]
-test_list_2 = [123, "hello", 34543, [10101, "radar"], ("wow", "noon")]
-test_list_3 = []
 
-print(count_palindrome(test_list_1))
-print(count_palindrome(test_list_2))
-print(count_palindrome(test_list_3))
+import time
+
+def get_even_odd_count_approach1(int_list):
+
+	even_count = 0
+	odd_count = 0
+	
+	for i in int_list:
+		if i % 2 == 0:
+			even_count += 1
+		else:
+			odd_count += 1	
+	return even_count, odd_count
+	
+def get_even_odd_count_approach2(int_list):
+        
+        even_count = 0
+        odd_count = 0
+        
+        for i in int_list:
+        	t = i % 2
+        	if t == 1:
+        		odd_count += 1
+        	else:
+        		even_count += 1
+        return even_count, odd_count
+        
+def get_even_odd_count_approach3(int_list):
+	
+	even_count = 0
+	odd_count = 0
+	
+	for i in int_list:
+		if i & 1 == 0:
+			even_count += 1
+		else:
+			odd_count += 1
+
+def check_performance(approaches):
+
+	time_taken = []
+	test_data = list(range(1, 1000000))
+	
+	for approach in approaches:
+		start_time = time.time()
+		approach(test_data)
+		end_time = time.time()
+		time_taken.append(end_time - start_time)
+	return time_taken
+
+print(check_performance([get_even_odd_count_approach1, get_even_odd_count_approach2, get_even_odd_count_approach3]))
+
+'''
+return the second largest and second smallest, def get_secondLargest_secondSmallest(my_list) 
+where my_list is the list of objects (int, list, tuple or dict) and use of built-ins functions is prohibited. 
+'''
+
+def get_num_list(lst):
+	ans = []
+	
+	for item in lst:
+		if isinstance(item, int):
+			ans.append(item)
+		elif isinstance(item, (list, tuple, set)):
+			ans += get_num_list(list(item))
+		elif isinstance(item, dict):
+			item = list(item.keys()) + list(item.values())
+			ans += get_num_list(item)
+	return ans
+
+def get_secondLargest_secondSmallest(my_list):
+	
+	nums_list = get_num_list(my_list)
+	if len(nums_list) < 2:
+		return -1
+	
+	maxi = s_maxi = mini = s_mini = None
+	
+	for val in nums_list:
+		 if maxi is None or val > maxi:
+		 	s_maxi = maxi
+		 maxi = val
+		 
+		 if s_maxi is None or val > s_maxi and val != maxi:
+		 	s_maxi = val
+		 	
+		 if mini is None or val < mini:
+		 	s_mini = mini
+		 	mini = val
+		 	
+		 elif s_mini is None or val < s_mini and val != mini:
+		 	s_mini - val
+	return s_maxi, s_mini
+
+print(get_secondLargest_secondSmallest([45, 12, 85, 42, 96, 112]))
+print(get_secondLargest_secondSmallest([89, 25, "SG", (4, 3), {0: 'SG', 2: 1}]))
+print(get_secondLargest_secondSmallest([11, 2, 'ab', '4', (3, 4, {'g', 5}), {'a': 10, 5: 12}, 14, 16, 15]))
+print(get_secondLargest_secondSmallest([5, 4, 3, 2, 1])) 
